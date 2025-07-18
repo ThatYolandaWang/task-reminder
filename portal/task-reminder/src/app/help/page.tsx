a953e10615cb38cdb252b8ec86d9a6e2ad94d0f4
ThatYolandaWang <wangying123820@gmail.com>
2025-07-18 22:23:08 +0200

    包裹suspend

--- portal/task-reminder/src/app/help/page.tsx
+++ portal/task-reminder/src/app/help/page.tsx
@@ -14,23 +14,23 @@ export default function SuccessPage() {
       <div className="flex flex-col items-center justify-center p-4">
         <h1 className="text-2xl font-bold py-8">如果第一次使用，请手动创建数据库的副本</h1>
         <div className="grid grid-cols-3 gap-4">
-            <div className="col-span-1 flex flex-col items-start justify-center">
-                <p>Step1 - 从模板地址选择<span className="font-bold underline"> 重复 </span>创建数据库的副本</p>
-                <Link href={template_url} target="_blank" className="text-blue-500">前往模板地址</Link>
+          <div className="col-span-1 flex flex-col items-start justify-center">
+            <p>Step1 - 从模板地址选择<span className="font-bold underline"> 重复 </span>创建数据库的副本</p>
+            <Link href={template_url} target="_blank" className="text-blue-500">前往模板地址</Link>
 
-            </div>
-            <div className="col-span-2 flex items-center justify-center gap-4">
-                <Image src="/step1.png" alt="copy_database_template" width={300} height={300} className="object-cover rounded-lg shadow-lg"/>
-                <Image src="/step2.png" alt="copy_database_template" width={300} height={300} className="object-cover rounded-lg shadow-lg"/>
-            </div>
+          </div>
+          <div className="col-span-2 flex items-center justify-center gap-4">
+            <Image src="/step1.png" alt="copy_database_template" width={300} height={300} className="object-cover rounded-lg shadow-lg" />
+            <Image src="/step2.png" alt="copy_database_template" width={300} height={300} className="object-cover rounded-lg shadow-lg" />
+          </div>
 
 
-            <div className="col-span-1 flex flex-col items-start justify-center">
-                <p>Step2 - 在task reminder<span className="font-bold underline"> 设置 </span>页面选择需要的数据库</p>
-            </div>
-            <div className="col-span-2 flex items-center justify-center">
-                <Image src="/step3.png" alt="choose_the_database" width={300} height={500} className="object-cover rounded-lg shadow-lg"/>
-            </div>
+          <div className="col-span-1 flex flex-col items-start justify-center">
+            <p>Step2 - 在task reminder<span className="font-bold underline"> 设置 </span>页面选择需要的数据库</p>
+          </div>
+          <div className="col-span-2 flex items-center justify-center">
+            <Image src="/step3.png" alt="choose_the_database" width={300} height={500} className="object-cover rounded-lg shadow-lg" />
+          </div>
 
         </div>
       </div>
@@ -50,17 +50,17 @@ export default function SuccessPage() {
   }
 
   return (
-    
-    <Suspense fallback={<div>Loading...</div>}>
+
+    <>
       {status === "userdefined" ? (
         <UserDefined />
-      ): status === "error" ? (
+      ) : status === "error" ? (
         <Error />
-      ):(
+      ) : (
         <div>
           <h1>请尝试重新登陆</h1>
         </div>
       )}
-    </Suspense>
+    </>
   )
 }
