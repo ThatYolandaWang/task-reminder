8d7ce26871942e0b88ac74fccc3e4a21710a77c5
ThatYolandaWang <wangying123820@gmail.com>
2025-07-19 01:17:49 +0200

    后端增加token校验的接口

---
+++ portal/task-reminder/src/app/api/notion/introspect/route.ts
@@ -0,0 +1,34 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  const token = body.token;
+
+  const NOTION_CLIENT_ID = process.env.NOTION_CLIENT_ID!;
+  const NOTION_CLIENT_SECRET = process.env.NOTION_CLIENT_SECRET!;
+  const NOTION_API_URL = process.env.NOTION_API_URL!;
+
+  const basicAuth = Buffer.from(
+    `${NOTION_CLIENT_ID}:${NOTION_CLIENT_SECRET}`
+  ).toString("base64");
+
+  const tokenRes = await fetch(`${NOTION_API_URL}/v1/oauth/introspect`, {
+    method: "POST",
+    headers: {
+      Authorization: `Basic ${basicAuth}`,
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      token,
+    }),
+  });
+
+
+  if (tokenRes.status === 200) {
+    const data = await tokenRes.json();
+    return NextResponse.json({ status: "success", data });
+  } else {
+    return NextResponse.json({ status: "error" });
+  }
+}
--- portal/task-reminder/src/app/help/page.tsx
+++ portal/task-reminder/src/app/help/page.tsx
@@ -2,7 +2,7 @@
 import { useSearchParams } from "next/navigation";
 import Image from "next/image"
 import Link from "next/link";
-import { Suspense } from "react";
+
 export default function SuccessPage() {
   const params = useSearchParams();
   const status = params.get("status");
